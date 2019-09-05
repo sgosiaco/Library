2dcec0d7a2f5e27d2deccee5dcd28f517c91b0ff
sgosiaco <sgosiaco@me.com>
2019-09-05 04:07:49 -0700

    Changed implementation to compile and added gson import in myapp.kt to fix gradle not including gson when building jar.

--- src/main/kotlin/io/github/sgosiaco/library/MyApp.kt
+++ src/main/kotlin/io/github/sgosiaco/library/MyApp.kt
@@ -2,5 +2,6 @@ package io.github.sgosiaco.library
 
 import io.github.sgosiaco.view.MainView
 import tornadofx.App
+import com.google.gson.* //not sure if needed to make sure it includes in jar or if compile was enough instead of implementation in build.gradle
 
 class MyApp: App(MainView::class, Styles::class)
